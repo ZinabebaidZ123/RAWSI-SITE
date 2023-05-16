8a6294865b5eb652febca538a77d6ce7ba78be1b
= <=>
2023-05-16 16:09:58 +0300

    hut

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,6 +20,7 @@
       }
     }
 
+
   /**
    * Easy selector helper function
    */
@@ -249,3 +250,21 @@
 
 
 })()
+
+
+Object.defineProperty(HTMLMediaElement.prototype, 'playing', {
+  get: function () {
+      return !!(this.currentTime > 0 && !this.paused && !this.ended && this.readyState > 2);
+  }});
+
+  $('body').on('click touchstart', function () {
+    const videoElement = document.getElementById('home_video');
+    if (videoElement.playing) {
+        // video is already playing so do nothing
+    }
+    else {
+        // video is not playing
+        // so play video now
+        videoElement.play();
+    }
+});
